a9b67f371dccfaf09d5349aa7a46fcacd0d2c3a5
nurio34 <nurioonsoftware@gmail.com>
2024-04-07 17:26:47 +0300

    typeAnimation

--- typeAnimation.js
+++ typeAnimation.js
@@ -1,1 +1,28 @@
 //TODO : type animation fonksiyonu oluştur.
+
+function typeAnimation(string, element) {
+    let index = -1;
+    let status = "increasing";
+
+    const interval = setInterval(() => {
+        if (index >= 0) {
+            element.textContent = string.slice(0, index);
+        }
+        if (index >= -1 && status === "increasing") {
+            index++;
+        }
+        if (index <= string.length + 1 && status === "decreasing") {
+            index--;
+        }
+
+        if (index === -1) status = "increasing";
+
+        if (index === string.length + 1) status = "decreasing";
+    }, 600);
+
+    element.classList.add("typeAnimation");
+}
+
+const p = document.querySelector(".p");
+
+typeAnimation("Hello World", p);
